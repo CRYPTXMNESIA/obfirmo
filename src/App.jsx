6cd24a4551346fb0dd6e0a080477a1ea3a96e664
CRYPTXMNESIA <82593272+CRYPTXMNESIA@users.noreply.github.com>
2024-06-10 06:34:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -458,6 +458,7 @@ function App() {
                   <li>- Free</li>
                   <li>- Easy to use</li>
                   <li>- Fully offline</li>
+                  <li>- built-in security breach checker (haveibeenpwned API)</li>
                   <li>- No data collection</li>
                   <li>- Most secure passwords</li>
                   <li>- You can install it as an app from the browser</li>
