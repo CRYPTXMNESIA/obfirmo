c11a696db6b4d840a706923b443cc0d317de7a71
CRYPTXMNESIA <82593272+CRYPTXMNESIA@users.noreply.github.com>
2024-06-11 23:37:20 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './App.css';
+import App from './App';
 import { registerSW } from 'virtual:pwa-register';
-
-const App = lazy(() => import('./App'));
 
 // Register the service worker with an onUpdate handler
 const updateSW = registerSW({
@@ -20,27 +18,6 @@ const updateSW = registerSW({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode style={{ backgroundColor: "#0b0d11" }}>
-    <Suspense fallback={
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            color: '#bebebe', // Adjust this color if needed
-            fontSize: '.70rem',
-            lineHeight: "1.2",
-            whiteSpace: "pre",
-            fontWeight: 'bold',
-        }}>
-            {`    __                ___            
-   / /___  ____ _____/ (_)___  ____ _
-  / / __ \\/ __ \`/ __  / / __ \\/ __ \`/
- / / /_/ / /_/ / /_/ / / / / / /_/ / 
-/_/\\____/\\__,_/\\__,_/_/_/ /_/\\__, /  
-                            /____/   `}
-        </div>
-    }>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>
 );
