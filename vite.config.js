e96fcf53c7625a4b62b04837b3f5e993f9d539a0
CRYPTXMNESIA <82593272+CRYPTXMNESIA@users.noreply.github.com>
2024-06-11 22:26:07 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -72,6 +72,33 @@ export default defineConfig({
         'Opera >= 20',
         'Samsung >= 5'
       ],
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
     })
   ],
   build: {
