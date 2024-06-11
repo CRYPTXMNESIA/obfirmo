8b7bbb6a7d6eb0f64d73ca64a2e26adb75df8d95
CRYPTXMNESIA <82593272+CRYPTXMNESIA@users.noreply.github.com>
2024-06-11 22:39:53 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import { registerSW } from 'virtual:pwa-register';
+
+const App = lazy(() => import('./App'));
 
 // Register the service worker with an onUpdate handler
 const updateSW = registerSW({
@@ -17,7 +19,9 @@ const updateSW = registerSW({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode style={{ backgroundColor: "black" }}>
-    <App />
+  <React.StrictMode style={{ backgroundColor: "#0b0d11" }}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
