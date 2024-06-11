fa47728abe6b57ae41052e7c585ad6b5173187c2
CRYPTXMNESIA <82593272+CRYPTXMNESIA@users.noreply.github.com>
2024-06-11 23:27:56 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -27,7 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             alignItems: 'center',
             height: '100vh',
             color: '#bebebe', // Adjust this color if needed
-            fontSize: '.75rem',
+            fontSize: '.70rem',
+            lineHeight: "1.2",
+            whiteSpace: "pre",
             fontWeight: 'bold',
         }}>
             {`    __                ___            
